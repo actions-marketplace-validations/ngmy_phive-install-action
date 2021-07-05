a88363172b6b3a57e7a7af4bf80f0eecc82ee307
Martin Kunitzsch <kuni@ostec.de>
2021-07-05 12:39:35 +0200

    fixed default setting of variable for phive installation

--- src/phive/install.sh
+++ src/phive/install.sh
@@ -1,9 +1,10 @@
 #/bin/bash
-
 set -Ceuo pipefail
+
+if [ "${1+defined}" = defined ]; then KEYSERVER=$1; else KEYSERVER=hkps://keyserver.ubuntu.com; fi
 
 wget -O phive.phar https://phar.io/releases/phive.phar
 wget -O phive.phar.asc https://phar.io/releases/phive.phar.asc
-gpg --keyserver $* --recv-keys 0x9D8A98B29B2D5D79
+gpg --keyserver ${KEYSERVER} --recv-keys 0x9D8A98B29B2D5D79
 gpg --verify phive.phar.asc phive.phar
 chmod +x phive.phar
