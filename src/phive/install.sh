bd4fa1d03474a054326e964e52779fa07b32b98b
Yuta Nagamiya <y.nagamiya@gmail.com>
2021-07-05 23:03:39 +0900

    Refactor

--- src/phive/install.sh
+++ src/phive/install.sh
@@ -1,10 +1,9 @@
 #/bin/bash
-set -Ceuo pipefail
 
-if [ "${1+defined}" = defined ]; then KEYSERVER=$1; else KEYSERVER=hkps://keyserver.ubuntu.com; fi
+set -Ceuo pipefail
 
 wget -O phive.phar https://phar.io/releases/phive.phar
 wget -O phive.phar.asc https://phar.io/releases/phive.phar.asc
-gpg --keyserver ${KEYSERVER} --recv-keys 0x9D8A98B29B2D5D79
+gpg --keyserver "$1" --recv-keys 0x9D8A98B29B2D5D79
 gpg --verify phive.phar.asc phive.phar
 chmod +x phive.phar
